1680cee53690c2a1912d6aa9cae695dd7a8d57e2
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-05-26 06:17:17 -0300

    ➕ Se crea archivo lista de compras y se trabaja con array anidado

---
+++ 03_listaDeCompras.js
@@ -0,0 +1,17 @@
+var miListaDeCompras = [["Cereal", 3], ["Leche", 2], ["Galletas", 4], ["Pan",5], ["Gaseosa", 7],["Pollo", 7]]
+
+console.log("Voy a comprar " + miListaDeCompras[0][1] + " unidades de " + miListaDeCompras[0][0]);  
+console.log("Voy a comprar " + miListaDeCompras[1][1] + " unidades de " + miListaDeCompras[1][0]);  
+console.log("Voy a comprar " + miListaDeCompras[2][1] + " unidades de " + miListaDeCompras[2][0]);  
+console.log("Voy a comprar " + miListaDeCompras[3][1] + " unidades de " + miListaDeCompras[3][0]);  
+console.log("Voy a comprar " + miListaDeCompras[4][1] + " unidades de " + miListaDeCompras[4][0]);  
+console.log("Voy a comprar " + miListaDeCompras[5][1] + " unidades de " + miListaDeCompras[5][0]);
+
+/*
+Voy a comprar 3 unidades de Cereal
+Voy a comprar 2 unidades de Leche
+Voy a comprar 4 unidades de Galletas
+Voy a comprar 5 unidades de Pan
+Voy a comprar 7 unidades de Gaseosa
+Voy a comprar 7 unidades de Pollo
+*/
