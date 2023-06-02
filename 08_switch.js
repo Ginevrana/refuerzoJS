5afaefffe990bff353caf4e9b8c73674bc48f9da
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-06-02 16:28:04 -0300

    ➕ Se agrega caso de volumen a switch

--- 08_switch.js
+++ 08_switch.js
@@ -22,3 +22,26 @@ function clasificarValor(valor) {
 }
 
 console.log(clasificarValor(8)); // nada
+
+//////////////////////////////////////////////////
+
+function clasificarVolumen(valor) {
+    var volumen;
+    switch (valor) {
+        case 1:
+            volumen = "bajo";
+            break;
+        case 2:
+        case 3:
+            volumen = "intermedio";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            volumen ="alto"
+            break;
+    }
+    return volumen;
+}
+
+console.log(clasificarVolumen(2));
